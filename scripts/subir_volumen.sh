e228b7b7ce265df4a8501c51f10ba1c1a056ca71
Efrain Vazquez <efra.chivas.evf@gmail.com>
2021-08-15 16:01:17 -0500

    started repository nordotfiles

---
+++ scripts/subir_volumen.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+card=$(pacmd list-sinks | awk '/* index:/{print $3}')
+
+if [ $card = 0 ]; then
+	pactl set-sink-volume 0 +5%
+elif [ $card = 1 ]; then
+	pactl set-sink-volume 1 +5%
+else
+	pactl set-sink-volume "$card" +5%
+fi
